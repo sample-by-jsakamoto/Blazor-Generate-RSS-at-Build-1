d101a101bf96ce51d2371aa7265eda0f9f07bb25
jsakamoto <junichi.jp@gmail.com>
2025-01-03 11:27:34 +0900

    Make all projects reference a shared library project

--- RSSFeedXMLFileGenerator/Program.cs
+++ RSSFeedXMLFileGenerator/Program.cs
@@ -5,29 +5,7 @@ using CommandLineSwitchParser;
 var option = CommandLineSwitch.Parse<CommandLineOptions>(ref args);
 
 // Set basic feed information
-var feed = new SyndicationFeed("Feed Title", "Feed Description", new Uri("http://example.com"));
-
-// Create feed items
-var items = new SyndicationItem[]
-{
-    new (
-        "Item 1 Title",
-        "Item 1 Content",
-        new Uri("http://example.com/item1"),
-        "Item 1 ID",
-        DateTimeOffset.Now
-    ),
-    new (
-        "Item 2 Title",
-        "Item 2 Content",
-        new Uri("http://example.com/item2"),
-        "Item 2 ID",
-        DateTimeOffset.Now
-    )
-};
-
-// Add items to the feed
-feed.Items = items;
+var feed = new SharedLibrary1.FeedGenerator().GenerateFeed();
 
 // Save the RSS feed to a XML file
 var feedPath = Path.GetFullPath(option.OutputPath ?? "feed.xml");
---
+++ SharedLibrary1/FeedGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.ServiceModel.Syndication;
+
+namespace SharedLibrary1;
+
+public class FeedGenerator
+{
+    public SyndicationFeed GenerateFeed()
+    {
+
+        // Set basic feed information
+        var feed = new SyndicationFeed("Feed Title", "Feed Description", new Uri("http://example.com"));
+
+        // Create feed items
+        var items = new SyndicationItem[]
+        {
+            new (
+                "Item 1 Title",
+                "Item 1 Content",
+                new Uri("http://example.com/item1"),
+                "Item 1 ID",
+                DateTimeOffset.Now
+            ),
+            new (
+                "Item 2 Title",
+                "Item 2 Content",
+                new Uri("http://example.com/item2"),
+                "Item 2 ID",
+                DateTimeOffset.Now
+            )
+        };
+
+        // Add items to the feed
+        feed.Items = items;
+
+        return feed;
+    }
+}
